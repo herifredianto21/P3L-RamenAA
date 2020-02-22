bbb31dd13246c94ddd68da82ff9df0b670c7754d
punyaangga <anggafantiya@gmail.com>
2020-02-22 12:15:04 +0700

    Template p3l

---
+++ Controller/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+$server="localhost";
+$username="root";
+$password='';
+$nama_db=''
+mysqli_connect('$server','$username','$password','$nama_db') or die("mysql_error()"); 
+?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<!-- Page untuk halaman utama -->
+
+</body>
+</html>
